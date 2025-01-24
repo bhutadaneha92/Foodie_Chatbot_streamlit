99596af4f6ae5efd23069d3ee7791e9b602a6d5c
bhutadaneha92 <167900739+bhutadaneha92@users.noreply.github.com>
2025-01-24 16:48:27 +0100

    Update chatbot_streamlit.py

--- chatbot_streamlit.py
+++ chatbot_streamlit.py
@@ -5,10 +5,11 @@ from langchain.chains import create_history_aware_retriever
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
 
-from API_key import Api_token
+#from API_key import Api_token
 import streamlit as st
 import os
-
+# Retrieve the API key from environment variables
+Api_token = os.getenv("HF_TOKEN")
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = Api_token
 
 # llm
